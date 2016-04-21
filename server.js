4821304c6ddede1bebbe8de6c9aa502fd131ce79
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-04-21 11:59:26 -0600

    static files.  duh.

--- server.js
+++ server.js
@@ -4,13 +4,11 @@
     , app = express()
     , path = require('path')
 
-  app.use('/:filePath', function(req, res, next) {
-  	res.sendFile(path.join(__dirname + req.params.filePath));
-  })
+  app.use(express.static(__dirname));
 
-  app.use('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-  });
+  // app.use('/', function(req, res, next) {
+  //   res.sendFile(path.join(__dirname + '/index.html'));
+  // });
 
   app.use(function (err, req, res, next) {
       res.send('error: ' + JSON.stringify(err));
